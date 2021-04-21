c67a18f48af321c0667ccc81f8d65c08be869c96
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-04-21 14:46:53 +0900

    up

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setuptools.setup(
     packages=setuptools.find_packages(where="aws_cdk_python"),
 
     install_requires=[
-        "aws-cdk.core==1.99.0",
+        "aws-cdk.core",
+        "aws-cdk.aws_ec2"
     ],
 
     python_requires=">=3.6",
